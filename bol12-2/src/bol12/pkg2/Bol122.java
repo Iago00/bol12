61e0f73419a46949533bfb7bd47dfaaeaa3131c8
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-12-12 12:47:58 +0100

    first commit

---
+++ bol12-2/src/bol12/pkg2/Bol122.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol12.pkg2;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol122 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Clase1 obj1= new Clase1(5,4); 
+         System.out.print(obj1.modificar(4)+" "); 
+         Clase1 obj2= new Clase1(5,4); 
+         System.out.print(obj2.modificar(5)+" "); 
+         obj2=obj1; 
+         System.out.println(obj2.modificar(5)+" "); 
+    }
+    
+}
